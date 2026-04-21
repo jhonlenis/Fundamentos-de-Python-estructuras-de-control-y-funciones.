cd3f502d03800fc82ab32339f99f9738d3f161ce
jhon alexander <jhonalexander0606@gmail.com>
2026-04-21 15:44:32 -0500

    tarea terminada seccion 1

--- Seccion 1/formato_salida.py
+++ Seccion 1/formato_salida.py
@@ -1,13 +1,46 @@
-print("Flechas duplicadas una al lado de la otra:")
-print("    * " * 2)
-print("   * * " * 2)
-print("  * * " * 2)
-print(" * * " * 2)
-print("*** *** " * 2)
-print("  * * " * 2)
-print("  * * " * 2)
-print("  ***** " * 2)
+# 1. Minimizar print() usando \n
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
 
-print("\n" + "-"*30 + "\n")
-print("Flecha generada con una sola instrucción print():")
-print("    *\n   * *\n  * *\n * *\n*** ***\n  * *\n  * *\n  *****")
+print("-" * 30)  # Separador
+
+# 2. Flecha doble de tamaño (manteniendo proporciones)
+print("        *")
+print("       * *")
+print("      *   *")
+print("     *     *")
+print("    *       *")
+print("   *         *")
+print("  *           *")
+print(" *             *")
+print("*****       *****")
+print("    *       *")
+print("    *       *")
+print("    *       *")
+print("    *       *")
+print("    *********")
+
+print("-" * 30)  # Separador
+
+# 3. Duplicar la flecha una al lado de la otra
+print("\nUsando multiplicacion de strings:")
+espacio = " " * 2
+print(("    *" + espacio) * 2)
+print(("   * *" + espacio) * 2)
+print(("  *   *" + espacio) * 2)
+print((" *     *" + espacio) * 2)
+print(("***   ***" + espacio) * 2)
+print(("  *   *" + espacio) * 2)
+print(("  *   *" + espacio) * 2)
+print(("  *****" + espacio) * 2)
+
+print("-" * 30)  # Separador
+
+# 4. Uso de apóstrofes en lugar de comillas
+print('    *')
+print('   * *')
+print('  *   *')
+print(' *     *')
+print('***   ***')
+print('  *   *')
+print('  *   *')
+print('  *****')
